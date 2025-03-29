30ea9c41f9c13214c91d7fea7938df0f39080b24
Ammaar Alam <ammaar@princeton.edu>
2025-03-28 23:03:08 -0400

    adding react dashboard

---
+++ room-draw-analysis/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import RoomDrawDashboard from './components/RoomDrawDashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <RoomDrawDashboard />
+    </div>
+  );
+}
+
+export default App;
